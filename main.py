f114e3fa410abba425c6c8b88038b4f2cb651d68
Red-Shura <136751558+Red-Shura@users.noreply.github.com>
2025-01-16 15:12:52 +0300

    123

--- main.py
+++ main.py
@@ -7,10 +7,13 @@ from telebot import types
 
 API_TOKEN = '7711719136:AAF2nw8_CLInKbO371tLuVf-HZICsHORX3A'
 bot = AsyncTeleBot(API_TOKEN)
+order_list = []
 
 
 @bot.message_handler(commands=['start'])
-async def handle_keyboard(message):
+async def hello_message(message):
+    bot.reply_to(message, 'Приветствую, я ТГ-бот сервис: Guzlobot.\n Способен принимать заказы от вас и выставлять их на доску объявлений.')
+async def handle_mainkeyboard(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     item = types.KeyboardButton("Мои заказы")
     markup.add(item)
@@ -22,13 +25,29 @@ async def handle_keyboard(message):
     markup.add(item)
 
     await bot.send_message(message.chat.id, "Выберите опцию:", reply_markup=markup)
-
-@bot.message_handler(func=lambda message: message.text == "Добавить заказ")
-async def say_order(message):
+'''Обработка команды добавление заказа'''
+@bot.message_handler(func=lambda message: message.text == "Добавить заказ") 
+async def receive_order(message):
     await bot.reply_to(message, "Описание заказа:")
-async def set_order(message):
-    order_range = message.text
-    order_author = message.chat.id
-    await bot.send_message(message.chat.id, order_author, order_range)
+    global orders
+    orders.append(message)
+    await bot.send_message("Ваш заказ принят!")
+    handle_mainkeyboard()
+    
+'''Обработка команды доска объявлений'''
+@bot.message_handler(func=lambda message: message.text == "Доска объявлений")
+async def show_orders():
+    if len(orders) > 0:
+        for order in orders:
+            await bot.send_message(order)
+    else:
+        await bot.send_message("Нет текущих заказов.")
+    handle_mainkeyboard()
+
+'''Обработка команды'''
+
+
+
+'''Обработка команды'''
 
 asyncio.run(bot.polling())
